672673039cbf2659273a64fe847358c509988b74
Rob Armes <robarmes@hotmail.com>
2018-07-09 14:36:57 +0100

    index

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,7 @@ router.use('/', require('./routes/questions'));
 
 router.get('/', function (req, res) {
 	req.session.destroy();
-	res.redirect('/app/dashboard');
+	res.render('index');
 });
 
 module.exports = router;
